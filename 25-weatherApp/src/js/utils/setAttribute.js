20fa57e6e6e810419f76a26dfafa54526a153d90
Suyud Setiawan Al Arif <suyudsetiawan@gmail.com>
2023-01-13 10:33:00 +0800

    Delete 25-weatherApp directory

--- 25-weatherApp/src/js/utils/setAttribute.js
+++
@@ -1,6 +0,0 @@
-const setAttribute = (newAttr) => {
-  document.documentElement.setAttribute('data-theme', newAttr);
-  localStorage.setItem('weatherTheme', newAttr);
-};
-
-export default setAttribute;
--- 25-weatherApp/src/js/views/toggleWeatherIcons.js
+++
@@ -1,4 +0,0 @@
-const sunIcon = '<i class="fa-solid fa-sun icon sun-icon"></i>';
-const moonIcon = '<i class="fa-solid fa-moon icon moon-icon"></i>';
-
-export { sunIcon, moonIcon };
